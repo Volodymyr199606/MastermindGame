213962106c8ba7882b3946fa90002197a24e9d9f
Volodymyr Behma <vbehma@vbehma-mn6016.linkedin.biz>
2025-01-05 14:12:21 -0800

    add constants to Configuration class

--- mastermind/src/main/java/mastermind/app/config/Configuration.java
+++ mastermind/src/main/java/mastermind/app/config/Configuration.java
@@ -1,4 +1,11 @@
 package mastermind.app.config;
 
-public class Configuration {
+public final class Configuration {
+  public static final int CODE_LENGTH = 4;
+  public static final int MAX_ATTEMPTS = 10;
+  public static final int MAX_DIGIT = 7;
+
+  // Private constructor to prevent instantiation
+  private Configuration() {
+  }
 }
