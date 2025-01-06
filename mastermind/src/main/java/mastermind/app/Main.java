e83254970b7aac78fa5f325b591b1863ca0002e2
Volodymyr Behma <vbehma@vbehma-mn6016.linkedin.biz>
2025-01-05 18:18:49 -0800

    add static method to Utils class

--- mastermind/src/main/java/mastermind/app/Main.java
+++ mastermind/src/main/java/mastermind/app/Main.java
@@ -10,6 +10,7 @@ public class Main {
   public static void main(String[] args) {
 
 
+    int maxAttempts = Utils.getMaxAttempts(args);
 
 
     InitDB initDB = new InitDB();
--- mastermind/src/main/java/mastermind/app/helpers/Utils.java
+++ mastermind/src/main/java/mastermind/app/helpers/Utils.java
@@ -1,5 +1,22 @@
 package mastermind.app.helpers;
 
+import mastermind.app.config.Configuration;
+
 
 public class Utils {
+
+  public static int getMaxAttempts(String[] args) {
+    int maxAttempts = Configuration.MAX_ATTEMPTS;
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("-t") && i + 1 < args.length) {
+        try {
+          maxAttempts = Integer.parseInt(args[i + 1]);
+        } catch (NumberFormatException e) {
+          System.out.println("Invalid number of attempts provided. Using default value: " + Configuration.MAX_ATTEMPTS);
+        }
+        break;
+      }
+    }
+    return maxAttempts;
+  }
 }
