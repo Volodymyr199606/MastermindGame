9f8a29e39b8780f10983872dd1f0d881a3fe90d9
Volodymyr Behma <vbehma@vbehma-mn6016.linkedin.biz>
2025-01-06 13:25:16 -0800

    add comments

--- mastermind/src/main/java/mastermind/app/helpers/Utils.java
+++ mastermind/src/main/java/mastermind/app/helpers/Utils.java
@@ -4,8 +4,20 @@ import java.util.Random;
 import mastermind.app.config.Configuration;
 
 
+/**
+ * The Utils class provides utility methods for the game.
+ * It includes methods to get the maximum number of attempts from command-line arguments
+ * and to generate a secret code for the game.
+ */
 public class Utils {
 
+  /**
+   * Retrieves the maximum number of attempts from the command-line arguments.
+   * If the argument is not provided or is invalid, it returns the default value from the configuration.
+   *
+   * @param args The command-line arguments
+   * @return The maximum number of attempts
+   */
   public static int getMaxAttempts(String[] args) {
     int maxAttempts = Configuration.MAX_ATTEMPTS;
     for (int i = 0; i < args.length; i++) {
@@ -21,6 +33,12 @@ public class Utils {
     return maxAttempts;
   }
 
+  /**
+   * Generates locally a secret code for the game.
+   * The secret code is a random sequence of digits within the specified range.
+   *
+   * @return The generated secret code as an array of characters
+   */
   public static char[] generateSecretCode() {
     Random random = new Random();
     char[] secretCode = new char[Configuration.CODE_LENGTH];
