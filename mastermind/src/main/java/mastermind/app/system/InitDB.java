a3a330e299b19e7c52f761a48c0d94eeef62ecdc
Volodymyr Behma <vbehma@vbehma-mn6016.linkedin.biz>
2025-01-09 14:32:13 -0800

    final refactoring

--- mastermind/src/main/java/mastermind/app/system/InitDB.java
+++ mastermind/src/main/java/mastermind/app/system/InitDB.java
@@ -1,12 +1,11 @@
 package mastermind.app.system;
 
-
-
 /**
  * Initializes the database by creating an instance of SQLiteDB and calling its connectAndInitialize method.
  */
 public class InitDB {
   public void init() {
+    System.out.println();
     System.out.println("Initializing database...");
     SQLiteDB db = new SQLiteDB();
     db.connectAndInitialize();
